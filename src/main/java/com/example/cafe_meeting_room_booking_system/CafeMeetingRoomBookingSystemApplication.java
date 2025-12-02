28837eefe0bb1664f351d9ee28287032e6407a32
UyChakriya <uychakriya002@gmail.com>
2025-12-02 17:23:26 +0700

    first commit

---
+++ src/main/java/com/example/cafe_meeting_room_booking_system/CafeMeetingRoomBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.cafe_meeting_room_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeMeetingRoomBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeMeetingRoomBookingSystemApplication.class, args);
+	}
+
+}
